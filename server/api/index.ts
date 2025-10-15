5389da8923831b3da177aaee18cbd326102cf67a
kuromaru-dotcom <gaijinkogarasumaru@gmail.com>
2025-10-15 10:14:52 +0800

    Delete server/api/index.ts

--- server/api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { getApp } from '../server/app';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const app = await getApp();
-  return app(req as any, res as any);
-}
